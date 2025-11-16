c741159866e17035cbf7489059c65e6f0611c51e
KirbyC <kirbdog315@gmail.com>
2025-11-15 20:47:19 -0600

    Added promptability

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
+import argparse
 
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-
-
+parser = argparse.ArgumentParser()
 
 def main():
     if sys.argv.__len__() < 2:
         print('Usage: uv run main.py <prompt>')
         sys.exit(1)
 
+    parser.add_argument("-v", "--verbose", help="Outputs response verbosely")
+
     user_prompt = sys.argv[1]
 
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=user_prompt
+        model='gemini-2.0-flash-001', 
+        contents=messages,
     )
 
     print(response.text)
